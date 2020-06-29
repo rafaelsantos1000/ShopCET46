6bb6923ddb7edde6e05d6e8ee4493007820ed13c
Rafael Santos <rafaasfs@gmail.com>
2020-06-29 15:45:16 +0100

    Add API products

---
+++ ShopCET46.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopCET46.Web.Data;
+
+namespace ShopCET46.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+
+        private readonly IProductRepository _productRepository;
+
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
