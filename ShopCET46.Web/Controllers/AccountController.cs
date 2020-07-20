049e03d7ed633db5b1757a1eb135c98d9967d641
Rafael Santos <rafaasfs@gmail.com>
2020-07-20 16:26:37 +0100

    Cascade Drop Down

--- ShopCET46.Web/Controllers/AccountController.cs
+++ ShopCET46.Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using ShopCET46.Web.Data.Entities;
+using ShopCET46.Web.Data.Repositories;
 using ShopCET46.Web.Helpers;
 using ShopCET46.Web.Models;
 using System;
@@ -17,15 +18,18 @@ namespace ShopCET46.Web.Controllers
     public class AccountController : Controller
     {
         private readonly IUserHelper _userHelper;
+        private readonly ICountryRepository _countryRepository;
         private readonly IConfiguration _configuration;
         private readonly IMailHelper _mailHelper;
 
         public AccountController(
             IUserHelper userHelper,
+            ICountryRepository countryRepository,
             IConfiguration configuration,
             IMailHelper mailHelper)
         {
             _userHelper = userHelper;
+            _countryRepository = countryRepository;
             _configuration = configuration;
             _mailHelper = mailHelper;
         }
@@ -77,7 +81,13 @@ namespace ShopCET46.Web.Controllers
 
         public IActionResult Register()
         {
-            return this.View();
+            var model = new RegisterNewUserViewModel
+            {
+                Countries = _countryRepository.GetComboCountries(),
+                Cities = _countryRepository.GetComboCities(0)
+            };
+
+            return this.View(model);
         }
 
 
@@ -89,12 +99,19 @@ namespace ShopCET46.Web.Controllers
                 var user = await _userHelper.GetUserByEmailAsync(model.Username);
                 if (user == null)
                 {
+
+                    var city = await _countryRepository.GetCityAsync(model.CityId);
+
                     user = new User
                     {
                         FirstName = model.FirstName,
                         LastName = model.LastName,
                         Email = model.Username,
-                        UserName = model.Username
+                        UserName = model.Username,
+                        Address = model.Address,
+                        PhoneNumber = model.PhoneNumber,
+                        CityId = model.CityId,
+                        City = city
                     };
 
                     var result = await _userHelper.AddUserAsync(user, model.Password);
@@ -157,13 +174,31 @@ namespace ShopCET46.Web.Controllers
         {
             var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
             var model = new ChangeUserViewModel();
+
             if (user != null)
             {
                 model.FirstName = user.FirstName;
                 model.LastName = user.LastName;
+                model.Address = user.Address;
+                model.PhoneNumber = user.PhoneNumber;
+
+                var city = await _countryRepository.GetCityAsync(user.CityId);
+                if (city != null)
+                {
+                    var country = await _countryRepository.GetCountryAsync(city);
+                    if (country != null)
+                    {
+                        model.CountryId = country.Id;
+                        model.Cities = _countryRepository.GetComboCities(country.Id);
+                        model.Countries = _countryRepository.GetComboCountries();
+                        model.CityId = user.CityId;
+                    }
+                }
             }
 
-            return View(model);
+            model.Cities = _countryRepository.GetComboCities(model.CountryId);
+            model.Countries = _countryRepository.GetComboCountries();
+            return this.View(model);
         }
 
 
@@ -171,31 +206,40 @@ namespace ShopCET46.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> ChangeUser(ChangeUserViewModel model)
         {
-            if (ModelState.IsValid)
+            if (this.ModelState.IsValid)
             {
                 var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 if (user != null)
                 {
+                    var city = await _countryRepository.GetCityAsync(model.CityId);
+
                     user.FirstName = model.FirstName;
                     user.LastName = model.LastName;
-                    var response = await _userHelper.UpdateUserAsync(user);
-                    if (response.Succeeded)
+                    user.Address = model.Address;
+                    user.PhoneNumber = model.PhoneNumber;
+                    user.CityId = model.CityId;
+                    user.City = city;
+
+                    var respose = await _userHelper.UpdateUserAsync(user);
+                    if (respose.Succeeded)
                     {
-                        ViewBag.UserMessage = "User update!";
+                        this.ViewBag.UserMessage = "User updated!";
                     }
                     else
                     {
-                        ModelState.AddModelError(string.Empty, response.Errors.FirstOrDefault().Description);
+                        this.ModelState.AddModelError(string.Empty, respose.Errors.FirstOrDefault().Description);
                     }
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "User not found!");
+                    this.ModelState.AddModelError(string.Empty, "User no found.");
                 }
             }
 
-            return View(model);
+            return this.View(model);
         }
+
+
 
 
         public IActionResult ChangePassword()
@@ -345,5 +389,13 @@ namespace ShopCET46.Web.Controllers
         {
             return View();
         }
+
+
+        public async Task<JsonResult> GetCitiesAsync(int countryId)
+        {
+            var country = await _countryRepository.GetCountryWithCitiesAsync(countryId);
+            return this.Json(country.Cities.OrderBy(c => c.Name));
+        }
+
     }
 }
